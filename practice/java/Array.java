0fd3c7bfc923217acf61580417be15d8537caa09
Kundan Kumar Sahu <espkundan@gmail.com>
2022-12-06 09:18:10 +0530

    Array Basic

---
+++ practice/java/Array.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+    // syntax
+    // datatype[] var_name = new datatype[size];
+    // Array of primitives
+    int[] arr = new int[5];
+
+    arr[0] = 1;
+    arr[1] = 2;
+    arr[2] = 3;
+    arr[3] = 4;
+    arr[4] = 5;
+
+    System.out.println(arr[2]);
+
+    //directly
+    // int[] rollNo = {1,2,3,4,5};
+
+    // for(int i = 0; i < arr.length; i++) {
+    //   arr[i] = input.nextInt();
+    // }
+
+    // print using for loop
+    // for(int i = 0; i < arr.length; i++) {
+    //   System.out.println(arr[i] + "");
+    // }
+
+    // for(int num : arr) { // for every element in the array print the element
+    //   System.out.println(num + ""); //here num represent elements of the array
+    // }
+
+    // System.out.println(Arrays.toString(arr)); 
+
+    //Array of objects
+    String[] str = new String[5];
+    
+    for(int i = 0; i < str.length; i++) {
+      str[i] = input.next();
+    }
+
+    System.out.println(Arrays.toString(str));
+  }
+}
--- practice/java/Main.java
+++ practice/java/Main.java
@@ -81,17 +81,17 @@ public static void main(String[] args) {
 
   /*Fibo */
   Scanner input = new Scanner(System.in);
-
+  
   int n = input.nextInt();
   int p = 0;
   int i = 1;
   int count = 2;
-
+  
   while(count <= n) {
-    int temp = i;
-    i = i + p;
-    p = temp;
-    count++;
+      int temp = i;
+      i = i + p;
+      p = temp;
+      count++;
   }
   System.out.println(i);
 
