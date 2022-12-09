3a73cb4867dc75573d0f72128da9d4d9cec1ca61
Kundan Kumar Sahu <espkundan@gmail.com>
2022-12-09 09:11:34 +0530

    Reverse array

--- practice/java/ArrList.java
+++ practice/java/ArrList.java
@@ -72,13 +72,30 @@ public class ArrList {
     // System.out.println();
 
     // System.out.println(multiAl);
+    
+    //swap
+    int[] arr = {1,2,3,4,5,6,7};
 
-    int[] arr = {1,2,3,4,5,6,7,8,9,10};
+    // swap(arr, 2, 6);
 
-    swap(arr, 2, 6);
+    reverse(arr);
 
     System.out.println(Arrays.toString(arr));
 
+    //max vlaue
+    // int[] arr = {1,2,3,4,5,6,7};
+
+  }
+
+  static void reverse(int[] arr) {
+    int start = 0;
+    int end = arr.length-1;
+
+    while(start < end) {
+      swap(arr, start, end);
+      start++;
+      end--;
+    }
   }
 
   static void swap(int[] arr, int index1, int index2) {
@@ -86,4 +103,14 @@ public class ArrList {
     arr[index1] = arr[index2];
     arr[index2] = temp;
     }
+
+  // static int max(int[] arr) {
+  //   int maxVal = arr[0];
+  //   for(int i = 1; i < arr.length; i++) {
+  //     if(arr[i] > maxVal) {
+  //       maxVal = arr[i];
+  //     }
+  //   }
+  //   return maxVal;
+  // }
 }
